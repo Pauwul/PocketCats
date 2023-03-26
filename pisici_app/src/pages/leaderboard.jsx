b72d02f7ff92fe6a60f2446f6a263db794c67c31
Paul Petrila <paul.petrila@adservio.ro>
2023-03-26 19:11:14 +0300

    Refactor folder structure, add prettier and babel rc

---
+++ pisici_app/src/pages/leaderboard.jsx
@@ -0,0 +1,11 @@
+function Leaderboard() {
+    return (
+      <div>
+        <h1>This is leaderboard page</h1>
+        <p>Here you can see top catlovers in your town</p>
+      </div>
+    );
+  }
+  
+  
+  export default Leaderboard;
