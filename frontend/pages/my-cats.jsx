ab6375a0daa73c2b3cbf0855b2bbbe5bb23eea4e
Edy <racovita.eduard2001@gmail.com>
2023-05-21 11:57:58 +0300

    Mock Gallery for user's cats

--- frontend/pages/my-cats.jsx
+++ frontend/pages/my-cats.jsx
@@ -1,21 +1,114 @@
-import styles from '../styles/Home_old.module.css';
-import withAuth from '../components/withAuth';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import styles from "../styles/Gallery.module.css";
 
-function MyCats() {
+const YourCatsGallery = () => {
+  const [images, setImages] = useState([]);
+  const [showAddCatForm, setShowAddCatForm] = useState(false);
+  const [catDetails, setCatDetails] = useState({
+    name: "",
+    breed: "",
+    photo: "",
+  });
+  const router = useRouter();
+
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    if (e.target.files.length) {
+      setCatDetails((prevState) => ({
+        ...prevState,
+        photo: e.target.files[0],
+      }));
+      setSelectedFile(e.target.files[0].name);
+    }
+  };
+
+  const handleAddImage = () => {
+    setShowAddCatForm(true);
+  };
+
+  const handleCloseForm = () => {
+    setShowAddCatForm(false);
+  };
+
+  const handleFormChange = (e) => {
+    setCatDetails({ ...catDetails, [e.target.name]: e.target.value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // TODO: handle the form submission
+    setShowAddCatForm(false);
+  };
+
   return (
-    <>
-    <div>
-      <h1>This is MyCats page</h1>
-      <p>Here is your PocketCats gallery</p>
-    </div>
-    <div className={styles.grid}>
-      <a href="/" className={styles.card}>
-            <h2>Home Page</h2>
-          </a>
+    <div className={styles.gallery}>
+      <h1 className={styles.title}>Your Cats Gallery</h1>
+      <button className={styles.addButton} onClick={handleAddImage}>
+        Add Image
+      </button>
+      {showAddCatForm && (
+        <div className={styles.modal}>
+          <form className={styles.form} onSubmit={handleFormSubmit}>
+            <button className={styles.closeButton} onClick={handleCloseForm}>
+              Close
+            </button>
+            <label className={styles.formLabel}>
+              Name:
+              <input
+                className={styles.formInput}
+                type="text"
+                name="name"
+                onChange={handleFormChange}
+                value={catDetails.name}
+                required
+              />
+            </label>
+            <label className={styles.formLabel}>
+              Breed:
+              <input
+                className={styles.formInput}
+                type="text"
+                name="breed"
+                onChange={handleFormChange}
+                value={catDetails.breed}
+                required
+              />
+            </label>
+            <label className={styles.formLabel}>
+              Photo:
+              <div className={styles.fileUploadContainer}>
+                <input
+                  className={styles.fileUploadInput}
+                  type="file"
+                  name="photo"
+                  onChange={handleFileChange}
+                  id="fileUpload"
+                  required
+                />
+                <label className={styles.fileUploadButton} htmlFor="fileUpload">
+                  {selectedFile ? selectedFile : "Choose file"}
+                </label>
+              </div>
+            </label>
+            <div className={styles.formButtonsContainer}>
+              <button className={styles.formSubmitButton} type="submit">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+      <div className={styles.imageContainer}>
+        {images.map((image, index) => (
+          <div key={index} className={styles.imageCard}>
+            <img className={styles.image} src={image} alt="Cat" />
+          </div>
+        ))}
       </div>
-    
-    </>
+    </div>
   );
-}
+};
 
-export default withAuth(MyCats);
+export default YourCatsGallery;
