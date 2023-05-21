dd6a6706729a4ac24142c87651ea715a920cfa46
Pauwul <petrila.paulcosmin@gmail.com>
2023-05-21 15:32:57 +0300

    Add better docs and better API endpoint output

--- cat-classificator/catdetector.py
+++ cat-classificator/catdetector.py
@@ -32,15 +32,23 @@ def predict():
         schema:
             id: prediction
             properties:
-                result:
+                breed:
                     type: string
+                    description: The breed of the cat, if it's not a cat, it may return something else, like a dog breed or other animal
+                    default: tabby
+                cat:
+                    type: boolean
                     description: Cat || Not a cat
-                    default: Cat
+                    default: false
+                probability:
+                    type: float
+                    description: The probability of the prediction
+                    default: 0.0
       404:
         description: Invalid input
       500:
         description: Error
-"""
+    """
     # Load the image from the request
     # img = load_img(request.files['image'], target_size=(224, 224))
 
@@ -54,14 +62,40 @@ def predict():
 
     # Make the prediction
     prediction = model.predict(img_array)
-
+    p = decode_predictions(prediction)
     # Return the result
     # if prediction[0][0][2] > 0.3 and (prediction[0][0][1] == 'tabby' or prediction[0][0][1] == 'tiger_cat' or prediction[0][0][1] == 'Egyptian_cat' ) :
-    print(prediction[0][0] )
-    if prediction[0][0] > 0.3 and (prediction[0][1] == 'tabby' or prediction[0][1] == 'tiger_cat' or prediction[0][1] == 'Egyptian_cat'):
-        return jsonify({'result': 'Not a cat'})
-    else:
-        return jsonify({'result': 'Cat'})
+
+
+
+
+    #  return only the breed and probability of each breed from the top 3 predictions
+
+    # cylce through the array of predictions and return the breed and probability of each breed from the top 3 predictions
+    array = []
+    for i in range(3):
+        breed = p[0][i][1]
+        if  breed in ['tabby', 'tiger_cat', 'Egyptian_cat']:
+            isCat = True
+        else:
+            isCat = False
+        probability = p[0][i][2]
+        array.append({'breed': f'{breed}', 'probability': f'{probability}', 'cat': f'{isCat}'})
+    # return each element as a json object
+    return jsonify(array)
+
+
+
+    # if breed == 'tabby' or breed == 'tiger_cat' or breed == 'Egyptian_cat' :
+    #     isCat = True
+    #     return jsonify({'cat': f'{isCat}', 'breed': f'{breed}'})
+
+    # return jsonify({'cat': f'{isCat}', 'breed': f'{breed}'})
+
+    # if p[0][2] > 0.3:
+    #     return jsonify({'result': f'{isCat}', 'breed': f'{breed}'})
+    # else:
+    #     return jsonify({'result': 'Not a cat', 'breed': f'{breed}'})
 
 if __name__ == '__main__':
     app.run(port=8000)
